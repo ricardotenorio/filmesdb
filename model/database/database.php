66c18d2a4d4f613c678cd26913f5c2b9b58a4bb4
ricardotenorio <ricardo.heber@gmail.com>
2018-11-16 13:02:02 -0200

    Adicionado a classe de conexão com o BD

---
+++ model/database/database.php
@@ -0,0 +1,35 @@
+<?php
+
+	class Database
+	{
+	    private static $dbName = 'filmesdb' ;
+	    private static $dbHost = 'localhost' ;
+	    private static $dbUsername = 'root';
+	    private static $dbUserPassword = 'root';
+	     
+	    private static $conn  = null;
+	     
+
+		#Singleton
+		private function __construct() {
+	        
+	    }
+	     
+	    public static function connect() {
+			if ( null == self::$conn ) {     
+		        try {
+					self::$conn =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);        
+		        }
+		        catch(PDOException $e) {
+					die($e->getMessage()); 
+		        }
+	       }
+	       return self::$conn;
+	    }
+	     
+	    public static function disconnect() {
+	        self::$conn = null;
+	    }
+	}	
+
+        
